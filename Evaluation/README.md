f5c6da38ae0789630c8c465634d2ea188071c226
crisarael <crisarael@gmail.com>
2022-05-19 17:37:07 -0700

    cambios

--- Evaluation/README.md
+++
@@ -1,8 +0,0 @@
-# Datos-Masivos
-
-## Practica evaluatoria la unidad 1 de la materia de datos masivos
-
-### Defensores de la practica:
-
-Garcia Torres Cristopher Arael #17210892
-Rivas Padilla Giselle #17210902
--- Practices/Practice_4/Test1GB.scala
+++ Practices/Practice_4/Test1GB.scala
@@ -1,76 +1,40 @@
-# Practice 4
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{GBTClassificationModel, GBTClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 
-# Exercise 1
-## The data is loaded.
-```scala
-//The data is loaded
+//Cargar datos
 val data = spark.read.format("libsvm").load("sample.txt")
-```
 
-# Exercise 2
-## Clean data
-```scala
-//Clean data and assign label and features
+//Limpiar datos y asignar label y features
 val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(data)
 val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(data)
-```
 
-# Exercise 3
-## Separate the array
-```scala
-// Separate the array into training and test data
+// Separar el arreglo en datos de entrenamiento y prueba
 val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
-```
 
-# Exercise 4
-## Define GBTClassifier model
-```scala
-// Define GBTClassifier model
+// Definir modelo GBTClassifier
 val gbt = new GBTClassifier().setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setMaxIter(10).setFeatureSubsetStrategy("auto")
-```
 
-# Note
-```scala
 // Traducir label indexados(No funciona en esta version?)
 //val labelConverter = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(labelIndexer.labelsArray(0))
-```
 
-# Exercise 5
-## Process order
-```scala
-//Define process order
+//Definir orden de proceso
 val pipeline = new Pipeline().setStages(Array(labelIndexer, featureIndexer, gbt))
-```
 
-# Exercise 6
-## The model is trained
-```scala
-// The model is trained using the training data
+// Se entrena el modelo utilizando los datos de entrenamiento
 val model = pipeline.fit(trainingData)
-```
 
-# Exercise 7
-## A test is performed
-```scala
-// A test is performed with the test data on the model
+// Se realiza un test con los datos de prueba en el modelo
 val predictions = model.transform(testData)
-```
 
-# Exercise 8
-## Show the results
-```sacla
-// Show the results obtained from the prediction
+// Mostrar los resultados obtenidos de la prediccion
 predictions.show(5)
-```
 
-# Exercise 9
-## Calculate the error
-```scala
-// Calculate the error
+// Calcular el error
 val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
 val accuracy = evaluator.evaluate(predictions)
 println(s"Test Error = ${1.0 - accuracy}")
 
 val gbtModel = model.stages(2).asInstanceOf[GBTClassificationModel]
 println(s"Learned classification GBT model:\n ${gbtModel.toDebugString}")
-```
